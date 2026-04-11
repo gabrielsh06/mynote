c846e8f611cf52b80c420c6d3f18ffa3791e1996
gabrielsh06 <shgabriel06@gmail.com>
2026-04-11 11:38:08 -0500

    Implement home directory retrieval

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    const char *home_dir = getenv("HOME");
+    if (home_dir == nullptr) {
+        fprintf(stderr, "HOME/ directory not found\n");
+        return EXIT_FAILURE;
+    }
+    printf("HOME/ found in: %s\n", home_dir);
+    return EXIT_SUCCESS;
 }
