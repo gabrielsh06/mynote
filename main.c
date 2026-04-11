640346ece08003c529c594d66646f19e14a86b74
gabrielsh06 <shgabriel06@gmail.com>
2026-04-11 17:03:01 -0500

    fead: add logic to open or create file

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(void) {
     const int len_dir_path = snprintf(nullptr, 0, "%s/.local/share/note", home_dir);
     char *dir_path = malloc(len_dir_path + 1);
     if (dir_path == nullptr) {
-        fprintf(stderr, "memory allocation failed\n");
+        fprintf(stderr, "memory allocation failed for dir_path\n");
         free(dir_path);
         return EXIT_FAILURE;
     }
@@ -32,6 +32,25 @@ int main(void) {
         fprintf(stdout, "directory created in: %s\n", dir_path);
     }
 
+    const int len_file_path = snprintf(nullptr, 0, "%s/notes.txt", dir_path);
+    char *file_path = malloc(len_file_path + 1);
+    if (file_path == nullptr) {
+        fprintf(stderr, "memory allocation failed for file_path\n");
+        free(dir_path);
+        free(file_path);
+        return EXIT_FAILURE;
+    }
+    snprintf(file_path, len_file_path + 1, "%s/notes.txt", dir_path);
+    FILE *file = fopen(file_path, "a");
+    if (file == nullptr) {
+        perror("failed to create or open file");
+        free(dir_path);
+        free(file_path);
+        return EXIT_FAILURE;
+    }
+
+    fclose(file);
+    free(file_path);
     free(dir_path);
     return EXIT_SUCCESS;
 }
