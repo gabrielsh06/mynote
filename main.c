9868ace3b1c0bef669af67da9e8978e0c93c47dd
gabrielsh06 <shgabriel06@gmail.com>
2026-04-11 15:09:58 -0500

    Add logic to create note directory

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 int main(void) {
     const char *home_dir = getenv("HOME");
@@ -7,6 +9,29 @@ int main(void) {
         fprintf(stderr, "HOME/ directory not found\n");
         return EXIT_FAILURE;
     }
+
     printf("HOME/ found in: %s\n", home_dir);
+
+    const int len_dir_path = snprintf(nullptr, 0, "%s/.local/share/note", home_dir);
+    char *dir_path = malloc(len_dir_path + 1);
+    if (dir_path == nullptr) {
+        fprintf(stderr, "memory allocation failed\n");
+        free(dir_path);
+        return EXIT_FAILURE;
+    }
+    snprintf(dir_path, len_dir_path + 1, "%s/.local/share/note", home_dir);
+    if (mkdir(dir_path, 0755) == -1) {
+        if (errno == EEXIST) {
+            fprintf(stdout, "directory exists in: %s\n", dir_path);
+        } else {
+            perror("error creating directory");
+            free(dir_path);
+            return EXIT_FAILURE;
+        }
+    } else {
+        fprintf(stdout, "directory created in: %s\n", dir_path);
+    }
+
+    free(dir_path);
     return EXIT_SUCCESS;
 }
