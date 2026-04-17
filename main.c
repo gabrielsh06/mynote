77fa071aebf4f55caad31c0a02482ff1a66eb612
gabrielsh06 <shgabriel06@gmail.com>
2026-04-16 23:25:39 -0500

    refactor: modularize file and path management

--- main.c
+++ main.c
@@ -2,15 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 
-static const char* const DIR_PATH = "/.local/share/note";
-static const char* const FILE_PATH = "/notes.txt";
+#include "paths.h"
+#include "storage.h"
 
-char* get_combined_path (const char *dir, const char *path);
-
-int main(int argc, char *argv[]) {
-    FILE *file = nullptr;
+int main(const int argc, char *argv[]) {
     char *file_path = nullptr;
     char *dir_path = nullptr;
     int status = EXIT_FAILURE;
@@ -19,82 +15,48 @@ int main(int argc, char *argv[]) {
         fprintf(stdout, "use: mynote \"your notes\"\n");
         fprintf(stdout, "     mynote --list\n");
         fprintf(stdout, "     mynote --clear\n");
+        status = EXIT_SUCCESS;
         goto cleanup;
     }
 
-    const char *home_dir = getenv("HOME");
-    if (!home_dir) {
-        fprintf(stderr, "HOME/ directory not found\n");
-        goto cleanup;
-    }
-
-    dir_path = get_combined_path(home_dir, DIR_PATH);
+    dir_path = get_dir_path();
     if (!dir_path) {
-        fprintf(stderr, "memory allocation failed for dir_path\n");
+        perror("memory allocation failed for dir_path");
         goto cleanup;
     }
-    if (mkdir(dir_path, 0755) == -1) {
-        if (errno != EEXIST) {
-            perror("error creating directory");
-            goto cleanup;
-        }
+
+    if (create_dir(dir_path)) {
+        goto cleanup;
     }
 
-    file_path = get_combined_path(dir_path, FILE_PATH);
+    file_path = get_file_path();
     if (!file_path) {
-        fprintf(stderr, "memory allocation failed for file_path\n");
+        perror("memory allocation failed for file_path");
         goto cleanup;
     }
 
-    file = fopen(file_path, "a");
-    if (!file) {
-        perror("failed to create or open file");
+
+    if (create_file(file_path)) {
         goto cleanup;
     }
 
     if (strcmp(argv[1], "--list") == 0) {
-        fclose(file);
-        file = fopen(file_path, "r");
-        if (!file) {
-            perror("failed to open file");
+        if (read_file(file_path)) {
             goto cleanup;
         }
-        char linea[256];
-        while (fgets(linea, sizeof(linea), file) != nullptr) {
-            printf("%s", linea);
-        }
     } else if (strcmp(argv[1], "--clear") == 0) {
-        fclose(file);
-        file = fopen(file_path, "w");
-        if (!file) {
-            perror("failed to clear file");
+        if (clear_file(file_path)) {
             goto cleanup;
         }
     } else {
-        fclose(file);
-        file = fopen(file_path, "a");
-        if (!file) {
-            perror("failed to create or open file");
+        if (write_file(file_path, argv[1])) {
             goto cleanup;
         }
-        fprintf(file, "%s\n", argv[1]);
     }
 
     status = EXIT_SUCCESS;
     cleanup:
-    if (file) fclose(file);
     free(file_path);
     free(dir_path);
     return status;
-}
-
-char* get_combined_path (const char *dir, const char *path) {
-    const int len = snprintf(nullptr, 0, "%s%s", dir, path);
-    if (len < 0) return nullptr;
-
-    char *full_path = malloc(len + 1);
-    if (full_path) {
-        snprintf(full_path, len + 1,"%s%s", dir, path);
-    }
-    return full_path;
 }
